7391619bf4ab0a242ff856798e86abde6ae19376
Armen Paronikyan <armparonikyan@gmail.com>
2017-12-15 21:49:46 +0400

    add turns functionality

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -7,55 +7,57 @@ const backImage = new Image();
 backImage.src = 'https://chupacdn.s3.amazonaws.com/catalog/product/cache/1/thumbnail/1280x/17f82f742ffe127f42dca9de82fb58b1/1/2/12-vector-game-backgrounds-8320_imgs_8320_4.png' ;
 
 const hero1 = new Image();
-hero1.src = 'http://www.pngall.com/wp-content/uploads/2016/06/Ninja-PNG-File.png';
+hero1.src = 'images/ninja.png';
 
 const hero2 = new Image();
-hero2.src='http://www.pngall.com/wp-content/uploads/2016/06/Ninja-PNG-File.png';
+hero2.src= 'images/ninja.png';
 
 const finish = new Image();
 finish.src='https://cdn0.iconfinder.com/data/icons/sports-solid-icons-3/48/110-512.png';
 
 
-  const gameData = {
-    hero: {
-      x: 20,
-      y: canvas.height-90,
-      img: hero1,
-      width: 80,
-      height: 80,
-      color: "red"
-    },  
-    finish:{
-      x:canvas.width-100,
-      y: canvas.height-100,
-      img:finish,
-      width: 100,
-      height: 100
-
-    },
-    hero2: {
-      x: 20,
-      y: canvas.height-80,
-      img: hero2,
-      width: 80,
-      height: 80,
-      color: "blue"
-    }
-  };
+const gameData = {
+	hero: {
+		x: 20,
+		y: canvas.height-90,
+		img: hero1,
+		width: 80,
+		height: 80,
+		color: "red"
+	},
+	finish:{
+		x:canvas.width-100,
+		y: canvas.height-100,
+		img:finish,
+		width: 100,
+		height: 100
+	},
+	hero2: {
+		x: 20,
+		y: canvas.height-80,
+		img: hero2,
+		width: 80,
+		height: 80,
+		color: "blue"
+	}
+};
 
 const drawhero = function() {
-  context.clearRect(0,0, canvas.width, canvas.height);
-  context.drawImage(backImage, 0, 0, canvas.width, canvas.height);
-  const hero = gameData.hero;
-  const finish=gameData.finish;
-  const hero2 =gameData.hero2;
+	context.clearRect(0,0, canvas.width, canvas.height);
+	context.drawImage(backImage, 0, 0, canvas.width, canvas.height);
+	const hero = gameData.hero;
+	const finish=gameData.finish;
+	const hero2 =gameData.hero2;
 
-  context.drawImage(hero.img, hero.x, hero.y, hero.width, hero.height); 
-  context.drawImage(finish.img,finish.x,finish.y,finish.width,finish.height)
-  context.drawImage(hero2.img, hero2.x, hero2.y, hero2.width, hero2.height); 
+	context.drawImage(hero.img, hero.x, hero.y, hero.width, hero.height);
+	context.drawImage(finish.img,finish.x,finish.y,finish.width,finish.height)
+	context.drawImage(hero2.img, hero2.x, hero2.y, hero2.width, hero2.height);
 };
- 
 
+const disableButtons = function(){
+	$(".button").addClass('disabled').attr('disabled', true);
+	myTurn = false;
+};
 
 
 const socket = io();
@@ -65,6 +67,7 @@ let history = '';
 let goal = 0;
 let myTurn = false;
 let sendValue = "no";
+let minusCount = 3;
 console.log('Heleluyah');
 
 socket.on('no space', data => {
@@ -83,12 +86,9 @@ socket.on('result', data =>{
 	const hero1=gameData.hero;
 	const hero2=gameData.hero2;
 	if(data.color===hero1.color){
-		hero1.x=hero2.x+((canvas.width-40)/goal)*parseInt(data.step[1]);
-	}
-	else {
-
-		hero2.x=hero1.x+((canvas.width-40)/goal)*parseInt(data.step[1]);
-		
+		hero1.x=hero2.x+((canvas.width-40)/goal)*parseInt(data.step[1]) * (data.step[0] === '+' ? 1  : -1);
+	} else {
+		hero2.x=hero1.x+((canvas.width-40)/goal)*parseInt(data.step[1]) * (data.step[0] === '+' ? 1  : -1);;
 	}
 	drawhero();
 	history += '<label style="color:'+ data.color +';">' + data.history.substring(data.history.length-2) + "</label>";
@@ -108,7 +108,7 @@ socket.on('start game', function(data) {
 socket.on('your turn', data => {
 	console.log("your turn worked");
 	$(".button").removeClass('disabled').attr('disabled', false);
-  myTurn = true;
+	myTurn = true;
 });
 
 $('.button').click((e) => {
@@ -116,14 +116,27 @@ $('.button').click((e) => {
 	$('.button').removeClass('active');
 	$(e.target).addClass('active');
 	console.log($(e.target).data('value'));
-  sendValue = $(e.target).data('value');
+	sendValue = $(e.target).data('value');
 });
 
 $('#plus').click(e => {
-  if (myTurn && sendValue !== 'no') {
-    console.log("+" + sendValue);
-    socket.emit('user step', "+" + sendValue);
-  }
+	if (myTurn && sendValue !== 'no') {
+		console.log("+" + sendValue);
+		socket.emit('user step', "+" + sendValue);
+		disableButtons();
+	}
+});
+
+$('#minus').click(e => {
+	if (myTurn && sendValue !== 'no' && minusCount > 0) {
+		console.log("-" + sendValue);
+		socket.emit('user step', "-" + sendValue);
+		minusCount--;
+		// if (minusCount === 0) {
+		// 	$('#minus').attr
+		// }
+		disableButtons();
+	}
 });
 
 
